286de0912570ed98003c099e652d4367bc0d7eb1
孔晨皓(鸥弋)(OhYee) <kongchenhao.kch@alibaba-inc.com>
2022-11-30 15:34:07 +0800

    v1.0.6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var getFormBody = require('body/form');
 var body = require('body');
 
 const BUILD_TIME = '{BUILD_TIME}';
-const VERSION = '1.0.5'; 
+const VERSION = '1.0.6'; 
 
 exports.handler = (req, resp, context) => {
   console.log('hello world');
